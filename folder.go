e4edd750d04f1e13d442c742c8da8439284fc25e
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-03-30 06:27:55 +0100

    Add support for copying and moving folders

--- folder.go
+++ folder.go
@@ -49,3 +49,63 @@ func (imgKit *ImageKit) DeleteFolder(folderPath string) (err error) {
 	_, err = imgKit.DoRequest(req)
 	return err
 }
+
+// Copy a folder.
+func (imgKit *ImageKit) CopyFolder(sourceFolderPath, destinationPath string) (jobId string, err error) {
+	reqBody := make(map[string]string)
+	reqBody["sourceFolderPath"] = sourceFolderPath
+	reqBody["destinationPath"] = destinationPath
+	reqBodyBytes, err := json.Marshal(reqBody)
+	if err != nil {
+		return "", err
+	}
+	req, err := http.NewRequest(
+		http.MethodPost,
+		fmt.Sprintf("%s/bulkJobs/copyFolder", BASE_URL),
+		bytes.NewBufferString(string(reqBodyBytes)),
+	)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	resBodyStr, err := imgKit.DoRequest(req)
+	if err != nil {
+		return "", err
+	}
+	responseFields := &map[string]string{}
+	err = json.Unmarshal([]byte(resBodyStr), responseFields)
+	if err != nil {
+		return "", err
+	}
+	return (*responseFields)["jobId"], nil
+}
+
+// Move a folder.
+func (imgKit *ImageKit) MoveFolder(sourceFolderPath, destinationPath string) (jobId string, err error) {
+	reqBody := make(map[string]string)
+	reqBody["sourceFolderPath"] = sourceFolderPath
+	reqBody["destinationPath"] = destinationPath
+	reqBodyBytes, err := json.Marshal(reqBody)
+	if err != nil {
+		return "", err
+	}
+	req, err := http.NewRequest(
+		http.MethodPost,
+		fmt.Sprintf("%s/bulkJobs/moveFolder", BASE_URL),
+		bytes.NewBufferString(string(reqBodyBytes)),
+	)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	resBodyStr, err := imgKit.DoRequest(req)
+	if err != nil {
+		return "", err
+	}
+	responseFields := &map[string]string{}
+	err = json.Unmarshal([]byte(resBodyStr), responseFields)
+	if err != nil {
+		return "", err
+	}
+	return (*responseFields)["jobId"], nil
+}
